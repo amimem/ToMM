023be79e4368396417ca292e8edfb71428331936
amimem <nil.objc@gmail.com>
2024-04-05 20:05:44 -0400

    edit outpur and error directories

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -4,12 +4,10 @@
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=80GB       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=0-6:00:00     # DD-HH:MM:SS
-#SBATCH -o /home/mila/m/memariaa/scratch/slurm-%j-%a.out
-#SBATCH -e /home/mila/m/memariaa/scratch/slurm-%j-%a.err
-#SBATCH --mail-type=ALL 
-#SBATCH --mail-user=memariaa@mila.quebec
+#SBATCH -o ~/scratch/slurm-%j-%a.out
+#SBATCH -e ~/scratch/slurm-%j-%a.err
 
-#SBATCH --array=0-12
+#SBATCH --array=0-1
 
 module load python/3.10.lua
 module load cudatoolkit/12.3.2
@@ -36,10 +34,10 @@ export WANDB_DIR=$SCRATCH/wandb
 # python data_gen.py --N 1000 --corr 0.0 --output $SCRATCH/output
 # python data_gen.py --N 10000 --corr 0.0 --output $SCRATCH/output
 
-# scratch/output/data_e72dd17cbc # 10
-# scratch/output/data_8cd3f1c6e8 # 100
-# scratch/output/data_025501a652 # 1000
-# scratch/output/data_66d5636af3 # 10000
+# scratch/output/data_75816d80b9 # 10
+# scratch/output/data_330fb6f722 # 100
+# scratch/output/data_45dd5039c4 # 1000
+# scratch/output/data_2a6573283c # 10000
 # python command_run.py
 
 echo "Starting task $SLURM_ARRAY_TASK_ID"
