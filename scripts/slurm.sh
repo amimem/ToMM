95992c2028e49908596ae81c586623811cd847a3
amimem <nil.objc@gmail.com>
2024-10-14 15:27:44 -0400

    correct path

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --gres=gpu:rtx8000:1       # Request GPU "generic resources"
-
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=1  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=8GB       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=0-02:00:00     # DD-HH:MM:SS
-#SBATCH --partition=unkillable
+#SBATCH --time=0-12:00:00     # DD-HH:MM:SS
+##SBATCH --partition=unkillable
 #SBATCH -o ./scratch/slurm_out/slurm-%j-%a.out
 #SBATCH -e ./scratch/slurm_out/slurm-%j-%a.err
 
-#SBATCH --array=0-7 # should match lines in commands.txt
+#SBATCH --array=1-6 # should match lines in commands.txt
 
 module load python/3.10.lua
 module load cudatoolkit/12.3.2
@@ -20,7 +19,7 @@ export WANDB_DIR=$SCRATCH/wandb
 # tar xf ~/projects/def-xxxx/data.tar -C $SLURM_TMPDIR/data
 
 echo "Starting task $SLURM_ARRAY_TASK_ID"
-command=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $HOME/abstraction/scripts/commands.txt)
+command=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $PWD/scripts/commands.txt)
 eval_command=$(eval echo $command)
 srun $eval_command
 
